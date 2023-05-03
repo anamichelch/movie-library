8bfbdc3db64f31608daa89b8662606a52b0cd517
anamichelch <ana.michelc13@gmail.com>
2023-05-03 15:05:15 -0500

    part one

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 My Personal Top 10 movie library, i can update, add, and delete records on this web page
 
 In This web page  you will find a list of my top movies, 
-It is connected to a SQL data base.
+It is connected to a SQL data base. 
+I am using a bootstrap template for the UI.
 
 You can add more movies as you watched them, edit the previous records and delete as well. 
 
--- main.py
+++ main.py
@@ -10,10 +10,57 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my-movies-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(1000), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+
+
+new_movie1 = Movie(
+    title="Ana Michel",
+    year=2012,
+    description="Publicist  Shepard finds himself trapped in a phone booth, pinned down by an extortionist's"
+                " sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads"
+                " to a jaw-dropping climax.",
+    rating=7.2,
+    ranking=9,
+    review="My favourite was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+
+with app.app_context():
+    db.create_all()
+    # db.session.add(new_movie1)
+    # db.session.commit()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    return render_template("index.html", all_movies=movies)
+
+
+@app.route("/edit")
+def edit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template("edit.html", form=form)
 
 
 if __name__ == '__main__':
